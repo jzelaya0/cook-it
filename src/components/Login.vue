781b8cb5e0d714937af44cf199466c85a6fc2b0e
Jesse Zelaya <jessezelaya1@gmail.com>
2018-07-04 12:51:59 -0700

    Created base login view

---
+++ src/components/Login.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <div class="login">
+    <h1>Sign In</h1>
+    <input type="email" name="email" placeholder="E-mail">
+    <input type="password" name="password" placeholder="Password">
+    <button type="button" name="button" v-on:click="login">Log In</button>
+    <p>Don't have an account? You can create one here</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data: () => {
+    return {}
+  },
+  methods: {
+    login: () => {
+      alert('hello')
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>

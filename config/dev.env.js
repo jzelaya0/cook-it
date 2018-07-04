216168821a7df562e3de57dbca87f72b04e57500
Jesse Zelaya <jessezelaya1@gmail.com>
2018-07-04 14:20:18 -0700

    Environmental variables set up and firebase install

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,7 +1,14 @@
 'use strict'
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
+const dotenv = require('dotenv')
+dotenv.config()
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  FIREBASE_APIKEY: JSON.stringify(process.env.FIREBASE_APIKEY),
+  FIREBASE_AUTHDOMAIN: JSON.stringify(process.env.FIREBASE_AUTHDOMAIN),
+  FIREBASE_DATABASEURL: JSON.stringify(process.env.FIREBASE_DATABASEURL),
+  FIREBASE_PROJECTID: JSON.stringify(process.env.FIREBASE_PROJECTID),
+  FIREBASE_MESSAGINGSENDERID: JSON.stringify(process.env.FIREBASE_MESSAGINGSENDERID)
 })
--- src/main.js
+++ src/main.js
@@ -3,8 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+// Initialize Firebase
+let config = {
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASEURL,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: "",
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID
+};
+firebase.initializeApp(config);
 
 /* eslint-disable no-new */
 new Vue({

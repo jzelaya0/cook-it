f9a37b88412df5a712523c9deb1e21ff8d0821cd
Jesse Zelaya <jessezelaya1@gmail.com>
2018-07-04 14:26:45 -0700

    Created login method with Firebase in login view

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,22 +1,35 @@
 <template lang="html">
   <div class="login">
     <h1>Sign In</h1>
-    <input type="email" name="email" placeholder="E-mail">
-    <input type="password" name="password" placeholder="Password">
+    <input v-model="email" type="email" name="email" placeholder="E-mail">
+    <input v-model="password" type="password" name="password" placeholder="Password">
     <button type="button" name="button" v-on:click="login">Log In</button>
     <p>Don't have an account? You can create one <router-link :to="{ path: 'sign-up'}">here</router-link></p>
   </div>
 </template>
 
 <script>
+import firebase from 'firebase'
+
 export default {
   name: 'login',
-  data: () => {
-    return {}
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
   },
   methods: {
-    login: () => {
-      alert('hello')
+    login() {
+      firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
+        (user) => {
+          alert('You are now connected')
+        },
+
+        (err) => {
+          alert(`Opps ${err.message}`)
+        }
+      )
     }
   }
 }

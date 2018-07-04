ca6ea5df2c05b7198fdeb7bd65005ecdb34fabe7
Jesse Zelaya <jessezelaya1@gmail.com>
2018-07-04 12:57:44 -0700

    Created base sign up view

---
+++ src/components/SignUp.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div class="sign-up">
+    <h1>Sign Up</h1>
+    <input type="email" name="email" placeholder="E-mail">
+    <input type="password" name="password" placeholder="Password">
+    <button type="button" name="sign-up">Sign Up</button>
+    <span>Go Login</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'signUp',
+  data: () => {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style lang="css">
+</style>

2620b64dda29ccb6a5c9f82b6371e612264043f1
Jesse Zelaya <jessezelaya1@gmail.com>
2018-07-04 14:21:50 -0700

    Set up user creation with Firebase

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,20 +1,37 @@
 <template lang="html">
   <div class="sign-up">
     <h1>Sign Up</h1>
-    <input type="email" name="email" placeholder="E-mail">
-    <input type="password" name="password" placeholder="Password">
-    <button type="button" name="sign-up">Sign Up</button>
+    <input v-model="email" type="email" name="email" placeholder="E-mail">
+    <input v-model="password" type="password" name="password" placeholder="Password">
+    <button v-on:click="signUp" type="button" name="sign-up">Sign Up</button>
     <p>Go back to <router-link :to="{ path: '/login' }">Sign In</router-link></p>
   </div>
 </template>
 
 <script>
+import firebase from 'firebase'
+
 export default {
   name: 'signUp',
-  data: () => {
-    return {}
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
   },
-  methods: {}
+  methods: {
+    signUp() {
+      firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(
+        (user) => {
+          alert('Your account has been created')
+          console.log(user);
+        },
+        (err) => {
+          alert(`Opps.. ${err.message}`)
+        }
+      )
+    }
+  }
 }
 </script>
 

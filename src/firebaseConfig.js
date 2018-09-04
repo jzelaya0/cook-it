01b3caf1cfbf066662a317e051a107e6b6783c9b
Jesse Zelaya <jessezelaya1@gmail.com>
2018-09-03 17:17:45 -0700

    Firebase storage bucket config added

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -7,7 +7,7 @@ let config = {
   authDomain: process.env.FIREBASE_AUTHDOMAIN,
   databaseURL: process.env.FIREBASE_DATABASEURL,
   projectId: process.env.FIREBASE_PROJECTID,
-  storageBucket: "",
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID
 };
 firebase.initializeApp(config)

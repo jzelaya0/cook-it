db892e28682132a3502233b666bf4944511054ad
Jesse Zelaya <jessezelaya1@gmail.com>
2018-09-03 17:18:16 -0700

    Vuex states for setting current user and setting user profile

--- src/store.js
+++ src/store.js
@@ -3,14 +3,28 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const firebase = require('./firebaseConfig.js')
+
 export const store = new Vuex.Store({
   state: {
-
+    currentUser: null,
+    userProfile: {}
   },
   actions: {
-
+    fetchUserProfile({ commit, state}) {
+      firebase.usersCollection.doc(state.currentUser.uid).get().then(res => {
+        commit('setUserProfile', res.data())
+      }).catch(err => {
+        console.log(err)
+      })
+    },
   },
   mutations: {
-
+    setCurrentUser(state, val) {
+      state.currentUser = val
+    },
+    setUserProfile(state, val) {
+      state.userProfile = val
+    },
   }
 })

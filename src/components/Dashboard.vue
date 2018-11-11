d27563d340de7401fe2ede1389acac182a890021
Jesse Zelaya <jessezelaya1@gmail.com>
2018-11-10 16:26:54 -0800

    Recipe list component
    Created base component for rendering recipes list in user dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,45 +1,34 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <button type="button" name="button" v-on:click="logout">Logout</button>
+  <div class="container">
+
+    <div class="row">
+      <div class="col-12">
+        <div class="jumbotron jumbotron-fluid">
+          <div class="container">
+            <h1>Hello, {{ userProfile.name }}</h1>
+          </div>
+        </div>
+      </div>
+      <div class="col-12">
+        <RecipeList v-bind:recipes="userRecipes"></RecipeList>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import firebase from 'firebase'
+import RecipeList from './RecipeList'
+import { mapState } from 'vuex'
+const firebase = require('../firebaseConfig.js')
 
 export default {
   name: 'Dashboard',
-  data () {
-    return {
-      msg: 'Welcome to Your Dashboard!'
-    }
+  components: { RecipeList },
+  computed: {
+    ...mapState(['userProfile', 'userRecipes'])
   },
   methods: {
-    logout() {
-      firebase.auth().signOut().then(() => {
-        this.$store.dispatch('clearData')
-        this.$router.replace('login');
-      })
-    }
+
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
---
+++ src/components/RecipeList.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div class="row">
+    <div class="col-12 col-md-6" v-for="recipe in recipes">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">{{ recipe.name }}</h5>
+          <p v-if="recipe.description" class="card-text">{{ recipe.description}}</p>
+          <button type="button" name="edit" class="btn btn-info">Edit</button>
+          <button type="button" name="edit" class="btn btn-danger">Delete</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RecipeList',
+  props: ['recipes']
+}
+</script>
+
+<style lang="css">
+</style>
